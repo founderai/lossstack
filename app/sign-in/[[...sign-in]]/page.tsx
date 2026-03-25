d0a946ef25d89f9ea46f99ed1aa07b6a68346f87
Isaac Cummings <isaacadjusts@gmail.com>
2026-03-24 21:26:56 -0500

    Fix: remove forceRedirectUrl from SignIn - Clerk reads redirect_url natively

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,18 +1,5 @@
 import { SignIn } from "@clerk/nextjs";
-import { currentUser } from "@clerk/nextjs/server";
-import { redirect } from "next/navigation";
 
-export default async function Page({
-  searchParams,
-}: {
-  searchParams: Promise<{ redirect_url?: string }>;
-}) {
-  const { redirect_url } = await searchParams;
-  const user = await currentUser();
-
-  if (user && redirect_url) {
-    redirect(redirect_url);
-  }
-
-  return <SignIn forceRedirectUrl={redirect_url} />;
+export default function Page() {
+  return <SignIn />;
 }
